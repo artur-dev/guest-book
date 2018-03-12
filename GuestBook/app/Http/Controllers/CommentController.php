5ffd5358e6d9165cf641d008ff14d72a30169fa4
Artur Siupik <artur.siupik@gmail.com>
2018-03-12 18:31:53 +0000

    storing data

--- GuestBook/app/Http/Controllers/CommentController.php
+++ GuestBook/app/Http/Controllers/CommentController.php
@@ -9,6 +9,8 @@ class CommentController extends Controller
 {
     /**
      * Lists most recent comments from guest book.
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
      */
     public function index()
     {
@@ -32,6 +34,13 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
+        // automatically goes back on failure
+        $validated_data = $request->validate([
+            'guest_name' => 'required|max:255',
+            'guest_email' => 'email|max:255',
+            'comment' => 'required'
+        ]);
 
+        Entries::create($validated_data);
     }
 }
